93cba01064ddfc1e5ed4fcf4d18b4278c804de66
DURJOYCKB <durjoyckb@gmail.com>
2025-06-28 22:48:44 +0600

    Day9 done

---
+++ Day9/conditional.js
@@ -0,0 +1,14 @@
+function isEven(number){
+    if(number % 2 === 0){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+console.log(isEven(7));
+console.log(isEven(204));
+console.log(isEven(185));
+console.log(isEven(964));
+console.log(isEven(2063));
+console.log(isEven(1));
