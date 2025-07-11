387ffc158e53ec9204def19c9823302cfe9107b5
DURJOYCKB <durjoyckb@gmail.com>
2025-07-11 20:44:13 +0600

    Day12 Done

---
+++ Day12/evens-only.js
@@ -0,0 +1,14 @@
+function evenNumbersOnly(numbers){
+    let evens = [];
+    for(const number of numbers){
+        if(number % 2 === 0){
+            console.log(number);
+            evens.push(number);
+
+        }
+    }
+    return evens;
+}
+const numbers =[4, 56, 77, 3, 88, 12];
+const evens = evenNumbersOnly(numbers);
+console.log('Even numbers are',evens);
