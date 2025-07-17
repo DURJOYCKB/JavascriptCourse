747623e2a0972590bceaecf95032e6ed269ac3da
DURJOYCKB <durjoyckb@gmail.com>
2025-07-17 21:28:11 +0600

    Day19 done

---
+++ Day19/max.js
@@ -0,0 +1,11 @@
+const Troy = 93;
+const Jack = 96;
+const Noah = 95;
+
+if(Troy > Jack && Troy > Noah){
+    console.log('Troy is the winner.');
+}else if(Jack > Troy && Jack > Noah){
+    console.log('Jack is the winner.');
+}else{
+    console.log('Noah is the winner.');
+}
