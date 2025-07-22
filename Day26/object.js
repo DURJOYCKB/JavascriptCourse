50b932276b72ea355c0cae92f5c8c6801ed88d22
DURJOYCKB <durjoyckb@gmail.com>
2025-07-22 20:46:46 +0600

    Day26 done

---
+++ Day26/object.js
@@ -0,0 +1,16 @@
+const student = {
+    Name : 'Durjoy Chakraborty',
+    Id : 1011,
+    Address : 'Rastar Matha',
+    IsMarried : false,
+    Car :{
+        Name : 'Porsche',
+        Price : 500000000,
+        Colour : 'Black',
+        Manufacturer :{
+            Name : 'Porsche',
+            CEO : 'Oliver Blume',
+        }
+    }
+}
+console.log(student);
