13ba7e5b1afc8190b8feb91c0f3e700b35165983
DURJOYCKB <durjoyckb@gmail.com>
2025-08-16 21:04:01 +0600

    Day37 done

---
+++ Day37/backtick.js
@@ -0,0 +1,8 @@
+const john = 'Myself John \n' +
+'I want to be a \n' +
+'Millionaire.\n';
+console.log(john);
+//default capture the whole string ..wihtout using any '+'
+const Durjoy = `Myself Durjoy
+I want to be a Billionaire.\n`;
+console.log(Durjoy);
