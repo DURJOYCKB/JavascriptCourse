48c5d905e970c4082b436fbdfb6496ca1c7ef93b
DURJOYCKB <durjoyckb@gmail.com>
2025-08-16 20:52:26 +0600

    Day36 done

---
+++ Day36/string.js
@@ -0,0 +1,11 @@
+const a = 10;
+const b = 20;
+
+const result = 'The sum of ' + a + ' and ' + b + ' is ' + ( a + b );
+console.log(result);
+
+const c = 30;
+const d = 40;
+
+const sum = `The sum of ${c} and ${d} is ${c+d}`;
+console.log(sum);
