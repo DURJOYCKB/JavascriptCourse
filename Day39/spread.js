bfc806f788df3541e73501ce629bc75ec9734550
DURJOYCKB <durjoyckb@gmail.com>
2025-08-18 21:02:10 +0600

    Day39 done

---
+++ Day39/spread.js
@@ -0,0 +1,11 @@
+const max = Math.max(23, 34, 65, 12, 77);
+
+console.log(max);
+
+//By using spread operator...
+
+const numbers = [1, 23, 77, 89, 45, 90, 101];
+const arraymax = Math.max(...numbers);
+
+console.log(arraymax);
+
