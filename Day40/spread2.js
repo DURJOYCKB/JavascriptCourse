9c7da24d93a9b467dc63bf361e8da117bbb3e77e
DURJOYCKB <durjoyckb@gmail.com>
2025-08-19 00:08:27 +0600

    Day40 done

---
+++ Day40/spread2.js
@@ -0,0 +1,17 @@
+const bondhu = [4, 5, 87, 9];
+const dost = bondhu;
+bondhu.push(20);
+
+console.log(bondhu);
+console.log(dost);
+
+
+//Using spread operator........
+
+const friends = [40, 15, 77, 29];
+const dosto = [...friends];
+friends.push(20);
+
+console.log(friends);
+console.log(dosto);
+
