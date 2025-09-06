9c6a96ab83f5423083bf3484651c722619bf6e30
DURJOYCKB <durjoyckb@gmail.com>
2025-09-06 18:02:51 +0600

    Day45 done

---
+++ Day45/chain.js
@@ -0,0 +1,14 @@
+const data1 = [{name: 'durjoy', address: 'chattogram'}]
+
+console.log(data1[0].address);
+
+const products={
+    count: 5000,
+    data: [
+        {id: 1,name: 'lenovo laptop', price: 65000},
+        {id: 2,name: 'macbook', price: 165000},
+    ] 
+}
+
+//Second id price
+console.log(products.data[1].price);
