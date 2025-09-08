95d645e3a1e35b40c5c0ea68253566f24ef63628
DURJOYCKB <durjoyckb@gmail.com>
2025-09-08 09:03:48 +0600

    Day49 done

---
+++ Day49/find.js
@@ -0,0 +1,7 @@
+const players = [23, 44, 65, 49, 74, 77, 32];
+
+const selected = players.find(p => p>60);
+const selected1 = players.find(q => q>70);
+
+console.log(selected);
+console.log(selected1);
