0da6d7aae7eba776e6b97bb50f20fb8867b501ce
DURJOYCKB <durjoyckb@gmail.com>
2025-09-13 21:08:30 +0600

    Day56 done

---
+++ Day56/triple.js
@@ -0,0 +1,20 @@
+const first = 2;
+const second = '2';
+
+if(first == second){
+    console.log('Values are equal');
+}
+else{
+    console.log('Values are not equal');
+}
+
+//Triple equal also check datatype
+const a = 2;
+const b = '2';
+
+if(a === b){
+    console.log('Values are equal');
+}
+else{
+    console.log('Values are not equal');
+}
