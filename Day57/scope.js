56229807676272ce8590f0494dff55bc14bfdd4d
DURJOYCKB <durjoyckb@gmail.com>
2025-09-14 21:31:36 +0600

    Day57 done

---
+++ Day57/scope.js
@@ -0,0 +1,12 @@
+function sum(num1, num2){
+    const result = num1 + num2;
+
+    if(true){
+        var result2= num1 - num2;
+        //console.log(result2);
+    }
+    console.log(result2);
+    console.log(result);
+}
+sum(20, 10);
+//console.log(result);
